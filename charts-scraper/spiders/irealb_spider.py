20452e739c44e3ef37770379ab1f747ee9b988c7
OrangePeelFX <florianrey.info@gmail.com>
2020-09-29 19:46:10 +0200

    make irealb spider

--- charts-scraper/spiders/irealb_spider.py
+++ charts-scraper/spiders/irealb_spider.py
@@ -1,20 +1,34 @@
 import scrapy
 
 
-class IrealbSpider(scrapy.Spider):
-    name = "irealb::charts"
+class IRealBSpider(scrapy.Spider):
+    name = 'irealb'
+    base_url = 'https://irealb.com/forums/'
 
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        yield scrapy.Request(url=self.base_url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        forums = [a.attrib['href'] for a in response.css('#forums .forumtitle a')]
+        for forum in forums:
+            yield scrapy.Request(f'{self.base_url}{forum}', self.parse_forum)
+
+    def next(self, response):
+        return list(set([a.attrib['href'] for a in response.css('.prev_next a[rel=next]')]))
+
+    def parse_forum(self, response):
+        print("parse_forum", response.url)
+        threads = [a.attrib['href'] for a in response.css('#threadlist .threadtitle a')]
+        for thread in threads:
+            yield scrapy.Request(f'{self.base_url}{thread}', self.parse_thread)
+
+        for forum in self.next(response):
+            yield scrapy.Request(f'{self.base_url}{forum}', self.parse_forum)
+
+    def parse_thread(self, response):
+        charts = [a.attrib['href'] for a in response.xpath("//a[starts-with(@href,'irealb')]")]
+        for chart in charts:
+            yield {"chart": chart}
+
+        for thread in self.next(response):
+            yield scrapy.Request(f'{self.base_url}{thread}', self.parse_thread)
